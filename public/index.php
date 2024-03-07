11a69cf580ff1fa7cd9d9faabdccd914430c5296
David B. Elliott <davidbelliott1@gmail.com>
2024-03-07 11:06:38 -0800

    adding inital code base

--- public/index.php
+++ public/index.php
@@ -2,4 +2,13 @@
 
 require '../helpers.php';
 
-require base_path('views/home.view.php');
+require basePath('Router.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
